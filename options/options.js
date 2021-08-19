9a8b9c9e92148809b7cb9ec4f0acb6f41ec1f2ae
Crowdbotics <team@crowdbotics.com>
2021-08-19 01:02:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "royal_bonus_29936Identifier",
+  url: "https://royal_bonus_29936Identifier.botics.co",
+  api: "https://royal_bonus_29936Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
